eb5013596af27912c20c25fa5520916af84e632c
Tyrese Luo <tyreseluo@outlook.com>
2025-02-04 22:34:22 +0800

    chore(analyzer): add clap deps

--- analyzer/src/main.rs
+++ analyzer/src/main.rs
@@ -1,4 +1,14 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[clap(
+  name = "makepad-analyzer",
+  version
+)]
+struct MakepadAnalyzer {}
+
 #[tokio::main]
 async fn main(){
+  MakepadAnalyzer::parse();
   makepad_lsp::start().await;
 }
