278d6a2ba218fa68e57eb0c226595b32d088fe8e
Tyrese Luo <tyreseluo@outlook.com>
2025-02-05 19:36:19 +0800

    chore: change some lib directory names

--- libs/makepad-analyzer-live/src/lib.rs
+++
@@ -1,8 +0,0 @@
-// This crate will do these things:
-
-// 1. parse live_design!{} blocks
-// 2. parse Live system keyworkds: pub/link/crate::/dep("")
-pub mod live_keywords_docs;
-// 3. parse hover information
-// 4. provide goto definition/completion
-// 5. Live System diagnostics
