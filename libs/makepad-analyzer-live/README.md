ebbe141e9a259b0ddfc4a4b2b6abbb60340641fe
Tyrese Luo <tyreseluo@outlook.com>
2025-02-05 12:19:18 +0800

    chore(libs): add makepad-analyzer-live crate

---
+++ libs/makepad-analyzer-live/README.md
@@ -0,0 +1,1 @@
+# Makepad Analyzer Live
---
+++ libs/makepad-analyzer-live/src/lib.rs
@@ -0,0 +1,7 @@
+// This crate will do these things:
+
+// 1. parse live_design!{} blocks
+// 2. parse Live system keyworkds: pub/link/crate::/dep("")
+// 3. parse hover information
+// 4. provide goto definition/completion
+// 5. Live System diagnostics
